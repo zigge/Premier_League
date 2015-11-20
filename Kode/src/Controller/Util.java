a5a8b8550d5bcdb1ba45b9b76eb843f493c8e232
zigge <lassebjorklund@lasses-MacBook-Pro.local>
2015-11-20 11:51:14 +0100

    Oprettet de forløbige klasser: Util, Keeper, Player.
    Jeg har defineret klasserne, men mangler stadig at oprette klasser for
    enums og for menu systemet.

---
+++ Kode/src/Controller/Util.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by lassebjorklund on 20/11/15.
+ */
+public class Util {
+}
---
+++ Kode/src/Model/Player.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by lassebjorklund on 20/11/15.
+ */
+public class Player {
+}
