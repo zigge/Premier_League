3c95a45476701585557cd7e3cdb149fab59df7fe
zigge <lassebjorklund@lasses-MacBook-Pro.local>
2015-11-20 14:32:47 +0100

    Updated files; Util, Keeper

--- Kode/src/Model/Keeper.java
+++ Kode/src/Model/Keeper.java
@@ -3,5 +3,9 @@ package Model;
 /**
  * Created by lassebjorklund on 20/11/15.
  */
-public class Keeper {
+public class Keeper extends Player {
+
+    public Keeper(String name, int salary, int position, String nationalaty) {
+        super(name, salary, position, nationalaty);
+    }
 }
