1c05ee07e2bc0ca99938747406097b64bb259779
zigge <lassebjorklund@lasses-MacBook-Pro.local>
2015-11-21 15:59:11 +0100

    Update util
     We need to look at some decisions for saving games and players. Should
    we decide where to put ‘em or should the user ?

--- Kode/project/src/Controller/Util.java
+++ Kode/project/src/Controller/Util.java
@@ -1,10 +1,27 @@
 package Controller;
 
+import java.io.File;
+import Model.Game;
+
 /**
  * Created by lassebjorklund on 20/11/15.
  */
 public class Util {
+    File savelocation = new File();
 
+//    TODO: Define if we determine the location of save games, or the user do!
+//    TODO: Complete file
+    public void saveGame(Game game){
+
+        if(System.getProperties("os.name").contains("Mac OS X")){ // Gets system name = Mac OS X. This is do to file system structure is different on operation systems.
+
+        }else if(System.getProperties("os.name").contains("Windows")){ // Gets system name = Windows
+
+        }else{ // Gets system name = Linux
+
+        }
+
+    }
 
     public enum fieldPosition{
         FORWARD("Forward position"),
