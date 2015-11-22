55e4b3f48730e065e9fbb483b86f29e4daf07d10
zigge <lassebjorklund@lasses-MacBook-Pro.local>
2015-11-22 15:03:39 +0100

    Added TODOs to Util

--- Kode/project/src/Controller/Util.java
+++ Kode/project/src/Controller/Util.java
@@ -8,34 +8,28 @@ import Model.Player;
 
 /**
  * Created by lassebjorklund on 20/11/15.
+ * TODO: Complete file
  */
 public class Util {
     File savelocation;
 
-//    TODO: Define if we determine the location of save games, or the user do!
-//    TODO: Complete file
+        //TODO: Define if we determine the location of save games, or the user do!
     public void saveGame(Game game){
-        //Some code to save "Game"
-        if(System.getProperty("os.name").contains("Mac OS X")){ // Gets system name = Mac OS X. This is do to file system structure is different on operation systems.
-        //Fill out code specific to OSX file system
-        }else if(System.getProperty("os.name").contains("Windows")){ // Gets system name = Windows
-        // Fill out code specific to Windows file system
-        }
-
+        //TODO Some code to save "Game"
     }
 
     public void savePlayer(ArrayList<Player> player){
-        //Some code to save "players"
+        //TODO Some code to save "players"
     }
 
     public Game loadGame(File locationOfGame){
-        //Some code to load "Game"
+        //TODO Some code to load "Game"
         return null;
     }
 
 
     public ArrayList<Player> loadPlayers(File locationOfPlayers){
-        //Some code to load "players"
+        //TODO Some code to load "players"
         return null;
     }
 
